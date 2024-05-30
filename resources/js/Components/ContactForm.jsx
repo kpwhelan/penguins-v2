ad26abc91183e1d2a7a41a7e513687faed0e6da6
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2024-05-29 21:02:23 -0400

    reformat membership text, add membership contact form

---
+++ resources/js/Components/ContactForm.jsx
@@ -0,0 +1,77 @@
+import { useForm } from "@inertiajs/react";
+import InputError from "./InputError";
+import InputLabel from "./InputLabel";
+import PrimaryButton from "./PrimaryButton";
+import TextInput from "./TextInput";
+
+export default function ContactForm({ }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <div>
+                <InputLabel htmlFor="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    name="name"
+                    value={data.name}
+                    className="mt-1 block w-full"
+                    autoComplete="name"
+                    // isFocused={true}
+                    onChange={(e) => setData('name', e.target.value)}
+                />
+
+                <InputError message={errors.name} className="mt-2" />
+            </div>
+
+            <div className="mt-4">
+                <InputLabel htmlFor="email" value="Email" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    className="mt-1 block w-full"
+                    autoComplete="email"
+                    onChange={(e) => setData('email', e.target.value)}
+                />
+
+                <InputError message={errors.email} className="mt-2" />
+            </div>
+
+            <div className="mt-4">
+                <InputLabel htmlFor="message" value="Message" />
+
+                <textarea
+                    id="message"
+                    name="message"
+                    value={data.message}
+                    autoComplete="message"
+                    onChange={(e) => setData('message', e.target.value)}
+                    rows="4"
+                    className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="">
+                    </textarea>
+
+
+                <InputError message={errors.message} className="mt-2" />
+            </div>
+
+            <div className="flex items-center justify-end mt-4">
+                <PrimaryButton className="ms-4" disabled={processing}>
+                    Submit
+                </PrimaryButton>
+            </div>
+        </form>
+    );
+}
--- resources/js/Pages/Membership.jsx
+++ resources/js/Pages/Membership.jsx
@@ -2,32 +2,50 @@ import WelcomePageNav from "@/Components/WelcomePageNav";
 import { Head } from "@inertiajs/react";
 import MembershipBackground from '../../../public/assets/membership-background.jpg';
 import MembershipPageInfoContainer from "@/Containers/MembershipPageInfoContainer";
+import ContactFormContainer from "@/Containers/ContactFormContainer";
+import ContactForm from "@/Components/ContactForm";
 
 export default function Membership() {
     return (
         <main>
-            <Head title="Granite State Penguins" />
+            <Head title="Membership" />
 
             <WelcomePageNav />
 
             <div className=" h-96 bg-membershipPageBackground bg-cover flex align-middle justify-center items-center">
-                <div className="text-center">
+                <div className="text-center bg-[#333333] bg-opacity-50 px-2">
                     <p className=" text-5xl">Membership</p>
-                    <p>&</p>
+                    <p className="text-3xl">&</p>
                     <p className="text-5xl">Equipment</p>
                 </div>
             </div>
 
-            <MembershipPageInfoContainer className="flex justify-around text-black p-4 mt-40">
-                <div className="w-[30%] flex items-center align-middle">
-
-                </div>
-
-                <div className="w-[50%]">
-                    <p className="text-2xl leading-relaxed">
+            <MembershipPageInfoContainer className="flex justify-around items-center text-black p-4 mt-36">
+                <div className="w-[40%]">
+                    {/* <p className="text-2xl leading-relaxed">
                         Membership rates are collected quarterly with the current rate set at $120 per quarter. We charge a $10 daily drop-in price that may be applied towards your first quarter membership. For insurance reasons, you are required to join US Masters Swimming (USMS) within 30 days, which is typically $55 depending on options. We’re so sure you’ll love swimming with the Granite State Penguins that we’d like to offer you a one month free trial period. USMS membership is not required for this trial period. Contact Chris Landry at CSL5@cwru.edu or 603-880-6303 for more information. Please fill out the following Penguins application before your first visit.
+                    </p> */}
+
+                    <p className="text-lg leading-relaxed">
+                        Our membership structure is designed to suit various preferences. Currently, our quarterly rate stands at $120, providing a convenient option for regular swimmers. Alternatively, we offer a $10 daily drop-in rate, which can be credited towards your first quarter membership if you choose to continue.
+                    </p>
+
+                    <p className="text-lg leading-relaxed mt-6">
+                        As part of our commitment to safety, joining US Masters Swimming (USMS) within 30 days is mandatory for insurance purposes, with membership fees typically around $55, depending on your chosen options.
+                    </p>
+
+                    <p className="text-lg leading-relaxed mt-6">
+                        To let you experience the excitement of swimming with the Granite State Penguins risk-free, we extend a complimentary one-month trial period, during which USMS membership is not required.
+                    </p>
+
+                    <p className="text-lg leading-relaxed mt-6">
+                        For more information or to get started, please fill out the contact form or feel free to contact Chris Landry at 603-880-6303. Don't forget to complete the Penguins application before your first visit. We look forward to welcoming you!
                     </p>
                 </div>
+
+                <ContactFormContainer className="w-[30%]">
+                    <ContactForm />
+                </ContactFormContainer>
             </MembershipPageInfoContainer>
         </main>
     );
