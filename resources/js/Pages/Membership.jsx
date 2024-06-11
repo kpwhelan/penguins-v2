4bdaf4fae2b1d3796e71a201c2e6ba98eec10a66
Kevin Whelan <kevinwhelandev@gmail.com>
2024-06-10 20:45:02 -0400

    add equipment section on membership page

--- resources/js/Pages/Membership.jsx
+++ resources/js/Pages/Membership.jsx
@@ -13,37 +13,48 @@ export default function Membership() {
             <WelcomePageNav />
 
             <div className=" h-96 bg-membershipPageBackground bg-cover flex align-middle justify-center items-center">
-                <div className="text-center bg-[#333333] bg-opacity-50 px-2">
-                    <p className=" text-5xl">Membership</p>
-                    <p className="text-3xl">&</p>
-                    <p className="text-5xl">Equipment</p>
+                <div className="text-center bg-[#333333] bg-opacity-30 px-2">
+                    <p className=" text-7xl">Membership & Equipment</p>
                 </div>
             </div>
 
-            <MembershipPageInfoContainer className="flex justify-around items-center text-black p-4 mt-36 mb-10">
-                <div className="w-[40%]">
-                    <p className="text-4xl mb-4">Membership</p>
+            <MembershipPageInfoContainer className=" text-black p-4 mt-32 mb-10">
+                <div className="flex justify-around items-center">
+                    <div className="w-[40%]">
+                        <p className="text-4xl mb-4">Membership</p>
 
-                    <p className="text-lg leading-relaxed">
-                        Our membership structure is designed to suit various preferences. Currently, our quarterly rate stands at $120, providing a convenient option for regular swimmers. Alternatively, we offer a $10 daily drop-in rate, which can be credited towards your first quarter membership if you choose to continue.
-                    </p>
+                        <p className="text-lg leading-relaxed">
+                            Our membership structure is designed to suit various preferences. Currently, our quarterly rate stands at $120, providing a convenient option for regular swimmers. Alternatively, we offer a $10 daily drop-in rate, which can be credited towards your first quarter membership if you choose to continue.
+                        </p>
 
-                    <p className="text-lg leading-relaxed mt-6">
-                        As part of our commitment to safety, joining US Masters Swimming (USMS) within 30 days is mandatory for insurance purposes, with membership fees typically around $55, depending on your chosen options.
-                    </p>
+                        <p className="text-lg leading-relaxed mt-6">
+                            As part of our commitment to safety, joining US Masters Swimming (USMS) within 30 days is mandatory for insurance purposes, with membership fees typically around $55, depending on your chosen options.
+                        </p>
 
-                    <p className="text-lg leading-relaxed mt-6">
-                        To let you experience the excitement of swimming with the Granite State Penguins risk-free, we extend a complimentary one-month trial period, during which USMS membership is not required.
-                    </p>
+                        <p className="text-lg leading-relaxed mt-6">
+                            To let you experience the excitement of swimming with the Granite State Penguins risk-free, we extend a complimentary one-month trial period, during which USMS membership is not required.
+                        </p>
 
-                    <p className="text-lg leading-relaxed mt-6">
-                        For more information or to get started, please fill out the contact form or feel free to contact Chris Landry at 603-880-6303. Don't forget to complete the Penguins application before your first visit. We look forward to welcoming you!
-                    </p>
+                        <p className="text-lg leading-relaxed mt-6">
+                            For more information or to get started, please fill out the contact form or feel free to contact Chris Landry at 603-880-6303. Don't forget to complete the Penguins application before your first visit. We look forward to welcoming you!
+                        </p>
+
+                        <div className="mt-10">
+                            <p className="text-4xl mb-4">Equipment</p>
+
+                            <p className="text-lg leading-relaxed">Practice Suit</p>
+                            <p className="text-lg leading-relaxed">Goggles</p>
+                            <p className="text-lg leading-relaxed">Most people bring fins</p>
+                            <p className="text-lg leading-relaxed">Some bring paddles</p>
+                            <p className="text-lg leading-relaxed">Note: pull buoys and kickboards are provided at the pool</p>
+                        </div>
+                    </div>
+
+                    <ContactFormContainer className="w-[30%]">
+                        <ContactForm />
+                    </ContactFormContainer>
                 </div>
-
-                <ContactFormContainer className="w-[30%]">
-                    <ContactForm />
-                </ContactFormContainer>
+                
             </MembershipPageInfoContainer>
         </main>
     );
