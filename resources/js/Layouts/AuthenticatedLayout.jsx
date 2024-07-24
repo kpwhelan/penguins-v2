6cb51fafc4c707f55706ea73d6a8937f16c8df7b
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2024-07-24 10:45:59 -0400

    add padding to top of navbar for authenicated layout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -10,7 +10,7 @@ export default function Authenticated({ user, header, children }) {
 
     return (
         <div className="min-h-screen ">
-            <nav>
+            <nav className='pt-1'>
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
