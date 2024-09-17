c7519c0cc668434e98973c83e133d56a18f66800
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2024-09-17 18:37:00 -0400

    video background fix

--- resources/js/Containers/WelcomePageContainer.jsx
+++ resources/js/Containers/WelcomePageContainer.jsx
@@ -5,9 +5,13 @@ export default function WelcomePageContainer({ children, className }) {
         <div className={className}>
             <div className='absolute w-auto min-w-full min-h-full max-w-none bg-[#2ac2f2] bg-opacity-20 z-10'></div>
             <div className='absolute w-auto min-w-full min-h-full max-w-none bg-black bg-opacity-50 z-20'></div>
-            <video src={WelcomeBackground}
-                autoPlay loop muted
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
                 className="absolute w-auto min-w-full min-h-full max-w-none object-cover brightness-75">
+                    <source src={WelcomeBackground} type='video/mp4'></source>
             </video>
 
             <div className="z-20">
