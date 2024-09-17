be7070bc1615e36df534806fab28ff048e7cd360
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2024-09-17 18:15:41 -0400

    fix autoplay for welcome page background video

--- resources/js/Containers/WelcomePageContainer.jsx
+++ resources/js/Containers/WelcomePageContainer.jsx
@@ -6,7 +6,7 @@ export default function WelcomePageContainer({ children, className }) {
             <div className='absolute w-auto min-w-full min-h-full max-w-none bg-[#2ac2f2] bg-opacity-20 z-10'></div>
             <div className='absolute w-auto min-w-full min-h-full max-w-none bg-black bg-opacity-50 z-20'></div>
             <video src={WelcomeBackground}
-                autoPlay="{true}" loop muted
+                autoPlay loop muted
                 className="absolute w-auto min-w-full min-h-full max-w-none object-cover brightness-75">
             </video>
 
